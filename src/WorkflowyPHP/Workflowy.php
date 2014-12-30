1bc0808e99d84c5ae26c3f134d2e93b82d2e959e
Johan <contact@johansatge.fr>
2014-12-30 19:53:58 +0100

    Copyright

--- src/WorkflowyPHP/Workflowy.php
+++ src/WorkflowyPHP/Workflowy.php
@@ -1,12 +1,6 @@
 <?php
 
-/*
- * This file is part of the WorkflowyPHP package.
- *
- * (c) Johan Satgé
- *
- * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
- */
+/* WorkflowyPHP - https://github.com/johansatge/workflowy-php */
 
 namespace WorkflowyPHP;
 
